ed6d5ceba6b40be10339864107c8cb6b2653d4d0
Sandra Corbacho Alonso <sandracorbacho@gmail.com>
2021-04-26 23:45:19 +0200

    seeds

---
+++ database/seeders/RolesTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Role;
+class RolesTableSeeder extends Seeder
+{
+    public function run()
+    {
+        $role = new Role();
+        $role->name = 'admin';
+        $role->description = 'Administrator';
+        $role->save();
+        $role = new Role();
+        $role->name = 'user';
+        $role->description = 'User';
+        $role->save();
+    }
+}
--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -1,30 +1,16 @@
 <?php
 
+
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use database\seeders\RoleTableSeeder;
 use App\Models\User;
 use App\Models\Role;
-class DatabaseSeeder extends Seeder
+
+class UserTableSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
     public function run()
     {
-      
-        $role = new Role();
-        $role->name = 'admin';
-        $role->description = 'Administrator';
-        $role->save();
-        $role = new Role();
-        $role->name = 'user';
-        $role->description = 'User';
-        $role->save();
-
         $role_user = Role::where('name', 'user')->first();
         $role_admin = Role::where('name', 'admin')->first();
         $user = new User();
@@ -39,5 +25,5 @@ class DatabaseSeeder extends Seeder
         $user->password = bcrypt('secret');
         $user->save();
         $user->roles()->attach($role_admin);
-    }
+     }
 }
